b676dbdced26596e889648b69058c54f6d89cf15
Zayn Jarvis <ZaynJarvis@gmail.com>
2019-03-12 11:44:39 +0800

    force deploy

--- src/QRPage/index.jsx
+++ src/QRPage/index.jsx
@@ -17,7 +17,7 @@ export default ({ showQR }) => (
 	>
 		<img
 			src="http://www.unitag.io/qreator/generate?crs=hQythUmB79bGc2uMmaA94NpuHBKNPHew2ymo4Pu7J3axReAf%252BQDoDwN4QD9DveYmQyxw9z3OOfZ3jl9X3ypOcei8gmbIaPN0cYrJmezRg5ZPjhuOrZZ%252FtopyvKvLL1eSYdxgKb7%252FbLCVI2bBYVZLgUeH471RmM%252Fsz5V0zo6MPkbJMgrYzea0%252FQ41fNPPC0aSRUcqqfSRz51dblMWAI5T%252FQ%253D%253D&crd=fhOysE0g3Bah%252BuqXA7NPQ0Oop2JxQVhDd8UvXwH8CPm6TSk%252Bwwg12Q3w825Gsbd0aFLBcdqkQ%252FgmzPF5dWpmfg%253D%253D"
-			alt="QR Code"
+			alt="Xperience App"
 		/>
 	</div>
 );
